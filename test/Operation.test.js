632fabb8379ff2ae3c028d4c534596540abe2258
Suraj Yadav <ysuraj877@gmail.com>
2022-07-01 13:45:52 +0530

    test Operation#value

--- test/Operation.test.js
+++ test/Operation.test.js
@@ -6,4 +6,9 @@ describe("Operation", function(){
 	const operation = new Operation(10, 20, "*")
 	expect(operation).to.be.instanceOf(Operation)
     })
+
+    it("should return value of operation", function(){
+	const operation = new Operation(10, 20, "*")
+	expect(operation.value()).to.equal(200)
+    })
 })
