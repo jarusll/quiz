4e2eaa4d289f5d8779938236b3e4501cc4264d79
Suraj Yadav <ysuraj877@gmail.com>
2022-07-01 14:23:43 +0530

    added RandomOperation#randomize

--- src/classes/RandomOperation.js
+++ src/classes/RandomOperation.js
@@ -3,11 +3,20 @@ const Math = require("./Math.js")
 const Array = require("./Array.js")
 
 class RandomOperation extends Operation {
+    operators = ["+", "-", "*", "/"]
     constructor(minAllowed, maxAllowed){
 	super()
+	this.minAllowed = minAllowed
+	this.maxAllowed = maxAllowed
 	this.left = Math.randomRange(minAllowed, maxAllowed)
 	this.right = Math.randomRange(minAllowed, maxAllowed)
-	this.operator = ["+", "-", "*", "/"].random()
+	this.operator = this.operators.random()
+    }
+
+    randomize(minAllowed, maxAllowed){
+	this.left = Math.randomRange(this.minAllowed, this.maxAllowed)
+	this.right = Math.randomRange(this.minAllowed, this.maxAllowed)
+	this.operator = this.operators.random()
     }
 }
 
