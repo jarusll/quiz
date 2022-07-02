7967bfe8f97636c34e163436135fecc1ad1bceeb
Suraj Yadav <ysuraj877@gmail.com>
2022-07-03 00:47:36 +0530

    added timer for countdown

--- src/components/QuizUI/index.js
+++ src/components/QuizUI/index.js
@@ -1,9 +1,19 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { useTimer } from 'use-timer';
 
 function QuizUI({ left, right, operator, submit }) {
 	const inputRef = useRef(null)
+	const { time, start } = useTimer({
+		initialTime: 10,
+		endTime: 0,
+		timerType: 'DECREMENTAL',
+		onTimeOver: () => submit("")
+	});
+	useEffect(() => start(), [])
+
 	return (
 		<div className="flex flex-column f2 items-center">
+			<pre>{time}</pre>
 			<div>{left} {operator} {right}</div>
 			<input type="text" ref={inputRef} />
 			<button className="f4 ma2 bn pa2 br1" onClick={() => {
