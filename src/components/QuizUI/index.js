0515e38fc2a0a1e65a276b511b349c095e8a75b5
Suraj Yadav <ysuraj877@gmail.com>
2022-07-02 22:16:04 +0530

    style UI

--- src/components/QuizUI/index.js
+++ src/components/QuizUI/index.js
@@ -3,10 +3,10 @@ import { useRef } from "react"
 function QuizUI({ left, right, operator, submit }) {
 	const inputRef = useRef(null)
 	return (
-		<div>
+		<div className="flex flex-column f2 items-center">
 			<div>{left} {operator} {right}</div>
 			<input type="text" ref={inputRef} />
-			<button onClick={() => {
+			<button className="f4 ma2 bg-blue white bn pa2 br1" onClick={() => {
 				submit(inputRef.current.value)
 				inputRef.current.value = ""
 			}}>Submit</button>
